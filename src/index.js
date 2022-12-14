37958d87132acf5fdfe55a716c01a09a75f7977c
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2022-12-14 16:23:33 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { MantineProvider } from '@mantine/core';
+import { CharacterAnimationsProvider } from './contexts/CharacterAnimations';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        globalStyles: (_theme) => ({
+          body: {
+            width: '100vw',
+            height: '100vh',
+          },
+          '#root': {
+            width: '100%',
+            height: '100%',
+          },
+        }),
+      }}
+    >
+      <CharacterAnimationsProvider>
+        <App />
+      </CharacterAnimationsProvider>
+    </MantineProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
